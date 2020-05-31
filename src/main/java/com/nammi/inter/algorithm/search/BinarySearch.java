0a693e23208749f434131cab818398146ba4cbe0
fornammi <qifangqian@163.com>
2020-05-31 17:13:08 +0800

    binary search

--- src/main/java/com/nammi/inter/algorithm/search/BinarySearch.java
+++ src/main/java/com/nammi/inter/algorithm/search/BinarySearch.java
@@ -1,21 +1,32 @@
 package com.nammi.inter.algorithm.search;
 
+/**
+ * 二分查找法
+ */
 public class BinarySearch {
 
+	/**
+	 * 二分查找-递归
+	 * @param a	待查找元素
+	 * @param arr 递增排序的数组
+	 * @param beginIndex
+	 * @param endIndex
+	 * @return	-1表示未找到
+	 */
 	public static int recursionBinarySearch(int a, int[] arr, int beginIndex, int endIndex){
-		if(arr==null || arr.length<=0
-				|| beginIndex<0 || endIndex<0
-				|| beginIndex>endIndex){
+		if(arr==null || arr.length<=0 || beginIndex<0 || endIndex<0 || beginIndex>endIndex){
 			return -1;
 		}
 		int midIndex = (beginIndex + endIndex) / 2;
-		if(a<arr[midIndex]){
-			return recursionBinarySearch(a, arr, beginIndex, midIndex-1);
-		}else if(a>arr[midIndex]){
-			return recursionBinarySearch(a, arr, midIndex+1, endIndex);
-		}else{
-			return midIndex;
+		if(a < arr[midIndex]) {
+			return recursionBinarySearch(a, arr, beginIndex, midIndex - 1);
 		}
+
+		if( a > arr[midIndex]) {
+			return recursionBinarySearch(a, arr, midIndex + 1, endIndex);
+		}
+
+		return midIndex;
 	}
 	
 	public static void main(String[] args) {
